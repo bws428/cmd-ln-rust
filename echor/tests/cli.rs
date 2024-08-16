02fb92f101520419c6b05bbb0546127d237e0987
Brian D. Wendt <briandwendt@gmail.com>
2024-08-16 13:39:46 -0400

    adding unit tests

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,16 +1,27 @@
+use anyhow::Result;
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
 
 #[test]
-fn dies_if_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_if_no_args() -> Result<()> {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
+    Ok(())
 }
 
 #[test]
 fn prints_hello() {
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello1() -> Result<()> {
+    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,6 +1,5 @@
 use assert_cmd::Command;
-
-fn main() {}
+use predicates::prelude::*;
 
 #[test]
 fn runs() {
