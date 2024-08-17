b7941ae7aa736ad010a82014642a2d03cb3dbb1b
Brian D. Wendt <briandwendt@gmail.com>
2024-08-17 15:58:40 -0400

    refactor main.rs

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -4,18 +4,23 @@ use predicates::prelude::*;
 use std::fs;
 
 #[test]
+fn prints_hello() {
+    // Using .unwrap() here can be dangerous if the
+    // input values aren't what you expect.
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("hello").assert().success();
+}
+
+#[test]
 fn dies_if_no_args() -> Result<()> {
+    // We'll replace .unwrap() with the `?` operator,
+    // to either unpack an Ok() value or propagate the Err()
+    // value to the return type.
     let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
     Ok(())
-}
-
-#[test]
-fn prints_hello() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
 }
 
 #[test]
